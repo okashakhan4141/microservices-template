446f5adeb39b43ef7d536e8bb7cba938f60030bc
okashakhan4141 <okashakhan4141@gmail.com>
2022-04-12 21:35:15 +0500

    sms services integrated

---
+++ sms/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+// import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@dstransaction/common";
+
+import { sendSmsRouter } from "./routes/sms";
+import { sendSmsParamsRouter } from "./routes/param-sms";
+import { sendOTPRouter } from "./routes/otp";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+    name: "session",
+  })
+);
+
+app.use(sendSmsRouter);
+app.use(sendSmsParamsRouter);
+app.use(sendOTPRouter);
+
+app.all("*", async (req, res) => {
+    throw new NotFoundError();
+});
+  
+app.use(errorHandler);
+  
+const start = async () => {    
+    app.listen(3000, () => {
+        console.log("SMS - Listening on port 3000");
+    });
+};
+  
+start();
---
+++ sms/src/routes/param-sms.ts
@@ -0,0 +1,22 @@
+import express from "express";
+const request = require('request');
+
+const router = express.Router();
+
+router.post("/api/sms/params", async (req, res) => {
+
+  const options = {
+    url: 'https://stagingsms.useboom.net/api/v1/message/send/params',
+    json: true,
+    headers: {
+        'authorization': `${req.headers['authorization']}`
+    },
+    body: req.body
+  };
+
+  request.post(options, (err, response, body) => {
+    err ? res.send(err) : res.send(body);
+  });
+});
+
+export { router as sendSmsParamsRouter };
